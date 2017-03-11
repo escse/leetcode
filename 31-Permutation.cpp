ad6b58314d108be5282c51b4ef019b8b5188ccee
escse <zjuzhenghao@gmail.com>
2017-03-11 16:29:18 +0800

    Solution31

--- 31-Permutation.cpp
+++ 31-Permutation.cpp
@@ -4,34 +4,39 @@
 using namespace std;
 
 class Solution {
-public:
-    void nextPermutation(vector<int>& nums) {
-        auto rfirst = nums.rbegin();
-        auto rlast  = nums.rend();
+  public:
+    void nextPermutation(vector<int> &nums) {
+        const auto rfirst = nums.rbegin();//reverse_iterator<vector<int>::iterator>(nums.end());
+        const auto rlast = nums.rend();//reverse_iterator<vector<int>::iterator>(nums.begin());
         auto pivot = next(rfirst);
-        while(pivot != rlast && *prev(pivot) <= *pivot)
+        while (pivot != rlast && *pivot >= *prev(pivot))
             ++pivot;
-        if (pivot == rlast)
+        if (pivot == rlast) {
             reverse(rfirst, rlast);
+            return;
+        }
         auto change = find_if(rfirst, pivot, bind1st(less<int>(), *pivot));
-        swap(*pivot, *change);
+        swap(*change, *pivot);
         reverse(rfirst, pivot);
     }
 };
 
 template <typename T>
-ostream& operator<< (ostream& cout, vector<T> vec) {
-    for_each(vec.begin(), vec.end(), [&](T& elem) {cout << elem << " ";});
+ostream &operator<<(ostream &cout, vector<T> vec)
+{
+    for_each(vec.begin(), vec.end(), [&](T &elem) { cout << elem << " "; });
     return cout;
 }
 
-int main() {
+int main()
+{
     vector<int> list;
-    for (int i = 0; i< 10; ++i) 
+    for (int i = 0; i < 2; ++i)
         list.push_back(i);
     auto sol = Solution();
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 10; i++)
+    {
         sol.nextPermutation(list);
-        cout << list << endl;    
+        cout << list << endl;
     }
 }
